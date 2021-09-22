639238eaf970c2951badcfed367a8811e0c1df23
blackspectre <theblackspectre@protonmail.com>
2021-09-22 10:36:32 +0100

    chore: remove customer controller and entity

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,4 @@
-// import { CustomerController } from './customer';
-// import { PingController } from './ping';
-import { CustomerController } from './customer';
 import { UserController } from './user';
 import EncryptPassword from '../utils/password-encrypt';
 
-export const CONTROLLERS = [new CustomerController(), new UserController(new EncryptPassword())];
+export const CONTROLLERS = [new UserController(new EncryptPassword())];
